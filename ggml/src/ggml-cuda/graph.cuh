be84e5c11465d56b9b547fd19f804f4fcec1f94d
James <jcrogers211@gmail.com>
2026-08-28 17:06:13 -0400

    fix(cuda): release graph copy pointer buffers

--- ggml/src/ggml-cuda/graph.cuh
+++ ggml/src/ggml-cuda/graph.cuh
@@ -23,6 +23,9 @@ struct ggml_cuda_graph {
         if (graph != nullptr) {
             CUDA_CHECK(cudaGraphDestroy(graph));
         }
+        if (dest_ptrs_d != nullptr) {
+            CUDA_CHECK(cudaFree(dest_ptrs_d));
+        }
     }
     uint64_t uid = 0;
     cudaGraph_t graph = nullptr;
@@ -41,7 +44,7 @@ struct ggml_cuda_graph {
     bool debug_mma_census_logged = false;
     bool use_cpy_indirection = false;
     std::vector<char *> cpy_dest_ptrs;
-    char ** dest_ptrs_d;
+    char ** dest_ptrs_d = nullptr;
     int dest_ptrs_size = 0;
     // Index to allow each cpy kernel to be aware of it's position within the graph
     // relative to other cpy nodes.
